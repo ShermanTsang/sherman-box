dda1936257635a075c7c1bac64054f19f4d55ecc
ShareManT <sharemant@163.com>
2019-05-03 18:52:55 +0800

    chore: change project structure

---
+++ pages/blog/_id.vue
@@ -0,0 +1,16 @@
+<style lang="scss">
+</style>
+
+<template>
+  <main>
+    blogShowPage
+  </main>
+</template>
+
+<script>
+export default {
+  validate({ params }) {
+    return /^\d+$/.test(params.id)
+  }
+}
+</script>
---
+++ pages/blog/index.vue
@@ -0,0 +1,17 @@
+<style lang="scss">
+</style>
+
+<template>
+  <main>
+    IndexPage
+    <br>
+    <nuxt-link to="/blog/1">
+      Blog1
+    </nuxt-link>
+  </main>
+</template>
+
+<script>
+export default {
+}
+</script>
