38184d129419241bf493a3559aca1ba857452521
ShareManT <sharemant@163.com>
2022-05-20 17:00:18 +0800

    feat(component): add enableSearch prop to Tag component

--- components/Tag.vue
+++ components/Tag.vue
@@ -2,36 +2,35 @@
 $prefix: 'tag';
 
 .#{$prefix} {
+  display: inline-block;
+
+  &__item {
     display: inline-block;
+    overflow: hidden;
+    position: relative;
+    font-size: .9rem;
+    margin: 2px;
+    padding: 6px 10px;
+    white-space: normal;
+    color: #666;
+    background-color: transparent;
+    border: 1px solid #ddd;
+    letter-spacing: 1px;
+    transition: all .2s ease-in;
+    line-height: 1.4;
+    border-radius: 2px;
 
-    &__item {
-      display: inline-block;
-      overflow: hidden;
-      position: relative;
-      cursor: default;
-      font-size: .9rem;
-      margin: 2px;
-      padding: 6px 10px;
-      white-space: normal;
-      color: #666;
-      background-color: transparent;
-      border: 1px solid #ddd;
-      letter-spacing: 1px;
-      transition: all .2s ease-in;
-      line-height: 1.4;
-      border-radius: 2px;
-
-      &:not(:last-child) {
-        margin-right: 5px;
-      }
-
-      &:hover {
-        border-color: transparent;
-        box-shadow: 1px 1px 10px rgba(177,177,177,.6);
-      }
+    &:not(:last-child) {
+      margin-right: 5px;
     }
 
+    &:hover {
+      border-color: transparent;
+      box-shadow: 1px 1px 10px rgba(177, 177, 177, .6);
+    }
   }
+
+}
 </style>
 
 <template>
@@ -40,8 +39,18 @@ $prefix: 'tag';
       <span>{{ tagLength }}</span>
     </template>
     <template v-else>
-      <span v-if="tagLength <= 1" class="tag__item"><slot /></span>
-      <span v-for="item in tagResult" v-else :key="item" class="tag__item">{{ item }}</span>
+      <span
+        v-if="tagLength <= 1"
+        :class="`tag__item ${enableSearch ? 'cursor-pointer' : 'cursor-default'}`"
+        @click="handleClick"
+      ><slot /></span>
+      <span
+        v-for="tag in tagResult"
+        v-else
+        :key="tag"
+        :class="`tag__item ${enableSearch ? 'cursor-pointer' : 'cursor-default'}`"
+        @click="handleClick(tag)"
+      >{{ tag }}</span>
     </template>
   </div>
 </template>
@@ -53,6 +62,11 @@ export default {
     count: {
       type: Boolean,
       default: false
+    },
+    enableSearch: {
+      type: Boolean,
+      default: false,
+      required: false
     }
   },
   data () {
@@ -66,6 +80,14 @@ export default {
     },
     tagResult () {
       return this.tagValue.split(',').filter(tag => tag !== '')
+    }
+  },
+  methods: {
+    handleClick (tag) {
+      if (this.enableSearch) {
+        const keyword = tag || this.$slots.default[0].text
+        this.$router.push({ name: 'search', query: { keyword } })
+      }
     }
   }
 }
